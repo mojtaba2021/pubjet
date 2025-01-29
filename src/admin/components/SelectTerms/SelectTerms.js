1bb93a908e89465fca75c9f1e9ef44ec408b023c
mojtaba <mojtaba.m2021@gmail.com>
2025-01-29 14:46:48 +0330

    Refactor : add max select item, select mode, allowClear , suffixIcon to SelectTerms Component

--- src/admin/components/SelectTerms/SelectTerms.js
+++ src/admin/components/SelectTerms/SelectTerms.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import PropTypes from 'prop-types';
 import styles from './SelectTerms.module.scss';
 import {findEndpointUrl, getAdminAjaxUrl, getAxios} from "../../../shared/scripts/utils";
@@ -7,8 +7,8 @@ import DebounceSelect from "../DebounceSelect/DebounceSelect";
 const axios = getAxios();
 
 const SelectTerms = props => {
-    const {selectProps, taxonomy, placeholder, value, onChange, className} = props;
-
+    const {selectProps, taxonomy, placeholder, value, onChange, className , mode  ,maxCount,required} = props;
+    const [error, setError] = useState(false);
     const fetchPosts = (keyword = '') => {
         return axios.get(getAdminAjaxUrl(), {
             params: {
@@ -16,30 +16,50 @@ const SelectTerms = props => {
                 search  : keyword,
                 taxonomy: taxonomy,
             },
-        }).then(response => response.payload);
+        }).then(response => {
+            return response.payload.map(option => ({
+                ...option,
+                disabled: value && value.length >= maxCount && !value.includes(option.value), // disable extra options
+            }));
+        });
+    };
+    const handleChange = (selected) => {
+        onChange(selected);
+        // if (required && (!selected || selected.length === 0)) {
+        //     setError(true);
+        // } else {
+        //     setError(false);
+        // }
     };
 
     return (
         <DebounceSelect
-            mode={'single'}
+            mode={mode}
             showSearch={true}
             value={value ? value : undefined}
             placeholder={placeholder}
-            onChange={onChange}
+            onChange={handleChange}
             className={`${className} ${styles.select}`}
             fetchOptions={fetchPosts}
+            allowClear={selectProps.allowClear || false}
+            suffixIcon={selectProps.suffixIcon}
+            // maxTagCount={'responsive'}
             {...selectProps}
+            status={ error && 'error'}
         />
+
     );
 };
 
 SelectTerms.propTypes = {
-    value      : PropTypes.oneOfType([PropTypes.array, PropTypes.string,]),
+    value      : PropTypes.oneOfType([PropTypes.array, PropTypes.string, PropTypes.number]),
     onChange   : PropTypes.func,
     selectProps: PropTypes.object,
     placeholder: PropTypes.string,
     className  : PropTypes.string,
     taxonomy   : PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+    mode       : PropTypes.string,
+    maxCount   : PropTypes.number,
 };
 
 SelectTerms.defaultProps = {
@@ -50,6 +70,8 @@ SelectTerms.defaultProps = {
     taxonomy   : '',
     onChange   : () => {
     },
+    mode       : 'single',
+    maxCount   : 10,
 };
 
 export default SelectTerms;
