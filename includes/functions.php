04ec0a37770a6068f18226b936622ad1bbac730c
mojtaba <mojtaba.m2021@gmail.com>
2025-03-05 12:10:57 +0330

    Add manual approve to request data in pubjet-info api for monitoring

--- includes/functions.php
+++ includes/functions.php
@@ -1604,6 +1604,7 @@ function pubjet_send_plugin_status_to_api($status) {
         'reportage_recipient_path' => pubjet_isset_value($settings['processDataByQueryString']) ? '?action=' . EnumActions::CreateReportage     : rest_get_url_prefix()     . '/pubjet/v1/reportage',
         'status_recipient_path'    => pubjet_isset_value($settings['processDataByQueryString']) ? '?action=' . EnumActions::PubjetStatus        : rest_get_url_prefix()     . '/pubjet/v1/status',
         'category_recipient_path'  => pubjet_isset_value($settings['processDataByQueryString']) ? '?action=' . EnumActions::PubjetCategories    : rest_get_url_prefix()     . '/pubjet/v1/site/categories',
+        'pubjet_data'              => [ 'manualApprove' => pubjet_isset_value($settings['manualApprove'],false)]
     ];
     pubjet_log($request_data);
     $response = wp_remote_post($url, [
