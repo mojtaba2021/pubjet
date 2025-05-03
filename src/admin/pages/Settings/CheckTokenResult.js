5548f6f3c7cb3929d72d9d37083d8c506ef8a13e
mojtaba <mojtaba.m2021@gmail.com>
2025-05-03 16:03:28 +0330

    Refactor: add description and alert visibility timeout

--- src/admin/pages/Settings/CheckTokenResult.js
+++ src/admin/pages/Settings/CheckTokenResult.js
@@ -1,50 +1,93 @@
 import React from 'react';
 import styles from './CheckTokenResult.module.scss';
-import {Alert, Space, Tooltip} from "antd";
-import {pubjet__} from "../../../shared/scripts/utils";
-import {LoadingOutlined, QuestionCircleOutlined} from "@ant-design/icons";
-import {connect} from "trim-redux";
-import {getStoreKey} from "./Actions";
+import { Alert, Space, Tooltip } from "antd";
+import { pubjet__ } from "../../../shared/scripts/utils";
+import { LoadingOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+import { connect } from "trim-redux";
+import { getStoreKey } from "./Actions";
 
-const CheckTokenResult = props => {
-    const {checkToken} = props;
-    if (!checkToken || 'undefined' === typeof checkToken || Object.keys(checkToken).length === 0) {
-        return;
-    }
-    const {checked, checking, valid} = props.checkToken;
-    const {website_id, website_url} = props.checkToken.payload;
-    if (!checked && !checking) {
-        return null;
-    }
-    if (checking) {
-        return <div className={styles.spinWrapper}>
-            <LoadingOutlined
-                spin={true}
-                style={{fontSize: '18px', marginBottom: '15px'}}
-            />
-        </div>;
-    }
-    const description = () => {
-        if (!valid) {
-            return pubjet__('invalid-token');
-        }
-        return <Space>
-            <span>{pubjet__('valid-token')}</span>
-            {website_id && <Tooltip title={`${website_id} - ${website_url}`}>
-                <QuestionCircleOutlined className={styles.cursorPointer}/>
-            </Tooltip>}
-        </Space>;
+class CheckTokenResult extends React.Component {
+    state = {
+        visible: true, // control visibility of alert
     };
-    return <div className={styles.wrapper}>
-        {
-            <Alert
-                type={checking ? 'info' : (valid ? 'success' : 'error')}
-                showIcon={true}
-                description={description()}
-            />
+
+    componentDidUpdate(prevProps) {
+        const { checkToken } = this.props;
+
+        if (checkToken.checked && !checkToken.checking && checkToken !== prevProps.checkToken) {
+            this.setState({ visible: true });
+            setTimeout(() => {
+                this.setState({ visible: false });
+            }, 10000);
         }
-    </div>
-};
+    }
+
+    render() {
+        const { checkToken } = this.props;
+        const { visible } = this.state;
+
+        if (!checkToken || typeof checkToken === 'undefined' || Object.keys(checkToken).length === 0) {
+            return null;
+        }
+
+        const { checked, checking, valid } = checkToken;
+        const { website_id, website_url } = checkToken.payload || {};
+
+        if (!checked && !checking) {
+            return null;
+        }
+
+        if (checking) {
+            return (
+                <div className={styles.spinWrapper}>
+                    <LoadingOutlined
+                        spin={true}
+                        style={{ fontSize: '18px', marginBottom: '15px' }}
+                    />
+                </div>
+            );
+        }
+
+        const message = () => {
+        
+            if(valid){
+                return (
+                    <Space>
+                        <span>{pubjet__('valid-token')}</span>
+                        {website_id && (
+                            <Tooltip title={`${website_id} - ${website_url}`}>
+                                <QuestionCircleOutlined className={styles.cursorPointer} />
+                            </Tooltip>
+                        )}
+                    </Space>
+                );
+            }
+           
+        };
+
+        const description = () => {
+            if (valid) {
+                return pubjet__('pubjet-triboon-connection-desc');
+            }else{
+                return pubjet__('invalid-token');
+            }
+        };
+
+        return (
+            <div className={styles.wrapper}>
+                {visible && (
+                    <Alert
+                        type={valid ? 'success' : 'error'}
+                        showIcon={true}
+                        description={description()}
+                        message={message()}
+                        style={{textAlign:'center'}}
+                    />
+                )}
+            </div>
+        );
+    }
+}
 
 CheckTokenResult.defaultProps = {
     checkToken: {},
