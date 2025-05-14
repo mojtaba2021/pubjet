4121c9c4eae095a73b5db6c82a911bc7a21cb90b
mojtaba <mojtaba.m2021@gmail.com>
2025-05-14 10:36:38 +0330

    Refactor : add tooltip and disable condition to misc tab

--- src/admin/pages/Settings/Settings.js
+++ src/admin/pages/Settings/Settings.js
@@ -4,7 +4,7 @@ import styles from './Settings.module.scss';
 import {connect} from "trim-redux";
 import Header from "./Header";
 import General from "./General";
-import {Tabs} from "antd";
+import {Tabs, Tooltip} from "antd";
 import Debug from "./Debug";
 import {pubjet__} from "../../../shared/scripts/utils";
 import Misc from "./Misc";
@@ -14,6 +14,13 @@ import SyncCategories from "./SyncCategories";
 import ModalReportageAuthor from "./ModalReportageAuthor";
 
 const Settings = props => {
+    const allHaveRelativeCategories =  props.options?.pricingPlans?.every(plan =>
+        Array.isArray(plan?.relative_categories) && plan?.relative_categories?.length > 0
+    );
+    const hasInvalidToken = !props.options.checkToken?.valid;
+    const hasIncompletePlans = !allHaveRelativeCategories;
+    const shouldDisableMiscTab = hasInvalidToken || hasIncompletePlans;
+
     return <div className={styles.container}>
         <Header/>
         <Tabs
@@ -41,8 +48,13 @@ const Settings = props => {
                     children: <RequiredPhpModules/>,
                 },
                 {
-                    key     : 'misc',
-                    label   : pubjet__('advanced'),
+                    key: 'misc',
+                    label: shouldDisableMiscTab ? (
+                        <Tooltip placement="top" title={'لطفا بروزرسانی تنظیمات را انجام دهید'} color='#ff4d4f'>
+                            <span>{pubjet__('advanced')}</span>
+                        </Tooltip>
+                    ) : pubjet__('advanced'),
+                    disabled: shouldDisableMiscTab,
                     children: <Misc/>,
                 },
             ]}
