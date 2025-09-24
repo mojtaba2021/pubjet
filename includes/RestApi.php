3d74068b64c2a35f2d18dbe93ddcdfd42c892f24
mojtaba <mojtaba.m2021@gmail.com>
2025-09-24 14:06:20 +0330

    Reformat code

--- includes/RestApi.php
+++ includes/RestApi.php
@@ -10,21 +10,24 @@ use triboon\pubjet\includes\traits\Utils;
 
 defined('ABSPATH') || exit;
 
-class RestApi extends Singleton {
+class RestApi extends Singleton
+{
 
     use Utils;
 
     /**
      * @return void
      */
-    public function init() {
+    public function init()
+    {
         add_action('rest_api_init', [$this, 'registerRoutes'], 15);
     }
 
     /**
      * @return void
      */
-    public function registerRoutes() {
+    public function registerRoutes()
+    {
         $this->registerRoute('reportage', 'createReportage', ['POST']);
         $this->registerRoute('reportage/(?P<reportageId>\d+)', 'updateReportage', ['PUT']);
         $this->registerRoute('reportage/(?P<reportageId>\d+)', 'findReportage', ['GET']);
@@ -43,14 +46,16 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function findSiteCategories() {
+    public function findSiteCategories()
+    {
         $this->success(pubjet_find_wp_categories(0, false));
     }
 
     /**
      * @return void
      */
-    public function findSiteTags(\WP_REST_Request $request) {
+    public function findSiteTags(\WP_REST_Request $request)
+    {
         $tags = pubjet_find_wp_tags();
         $this->success($tags);
     }
@@ -112,25 +117,27 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function findReportage(\WP_REST_Request $request) {
+    public function findReportage(\WP_REST_Request $request)
+    {
         $reportage_id = $request->get_param('reportageId');
         pubjet_log('===== Get Reportage Post =====');
         $reportage_post_id = pubjet_find_post_id_by_reportage_id($reportage_id);
-        $reportage_post    = get_post($reportage_post_id);
+        $reportage_post = get_post($reportage_post_id);
         if (!$reportage_post_id || empty($reportage_post)) {
             $this->error(pubjet__('post-not-found'), 404);
         }
         $this->success([
-                           'postId'    => $reportage_post->ID,
-                           'postTitle' => $reportage_post->post_title,
-                           'postUrl'   => get_permalink($reportage_post->ID),
-                       ]);
+            'postId' => $reportage_post->ID,
+            'postTitle' => $reportage_post->post_title,
+            'postUrl' => get_permalink($reportage_post->ID),
+        ]);
     }
 
     /**
      * @return void
      */
-    public function deleteReportage(\WP_REST_Request $request) {
+    public function deleteReportage(\WP_REST_Request $request)
+    {
         $reportage_id = $request->get_param('reportageId');
         pubjet_log("==== Delete Reportage Post ====");
         pubjet_log($reportage_id);
@@ -147,32 +154,33 @@ class RestApi extends Singleton {
         }
 
         $reportage_post = get_post($reportage_post_id);
-        $result         = wp_delete_post($reportage_post_id, true);
+        $result = wp_delete_post($reportage_post_id, true);
         if (is_wp_error($result)) {
             $this->error($result->get_error_message(), 500);
         }
 
         $this->success([
-                           'postId'      => absint($reportage_post_id),
-                           'postTitle'   => $reportage_post->post_title,
-                           'reportageId' => absint(pubjet_isset_value($reportage_id)),
-                       ]);
+            'postId' => absint($reportage_post_id),
+            'postTitle' => $reportage_post->post_title,
+            'reportageId' => absint(pubjet_isset_value($reportage_id)),
+        ]);
     }
 
     /**
      * @return void
      */
-    public function findSiteInfo(\WP_REST_Request $request) {
+    public function findSiteInfo(\WP_REST_Request $request)
+    {
         /**
          * The pubjet_siteinfo filter.
          *
          * @since 1.0.0
          */
         $result = apply_filters('pubjet_siteinfo', [
-            'title'          => get_bloginfo('name'),
-            'description'    => get_bloginfo('description'),
-            'site_url'       => get_bloginfo('wpurl'),
-            'site_version'   => get_bloginfo('version'),
+            'title' => get_bloginfo('name'),
+            'description' => get_bloginfo('description'),
+            'site_url' => get_bloginfo('wpurl'),
+            'site_version' => get_bloginfo('version'),
             'pubjet_version' => PUBJ()->getVersion(),
         ]);
         $this->success($result);
@@ -181,7 +189,8 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function toggleCopyright(\WP_REST_Request $request) {
+    public function toggleCopyright(\WP_REST_Request $request)
+    {
         pubjet_log('==== Change Copyright Status ====');
 
         $reportage_post_id = pubjet_find_post_id_by_reportage_id($request->get_param('reportageId'));
@@ -199,10 +208,10 @@ class RestApi extends Singleton {
         }
 
         $this->success([
-                           'postId'      => $reportage_post_id,
-                           'reportageId' => $request->get_param('reportageId'),
-                           'status'      => $new_status,
-                       ]);
+            'postId' => $reportage_post_id,
+            'reportageId' => $request->get_param('reportageId'),
+            'status' => $new_status,
+        ]);
     }
 
     /**
@@ -210,23 +219,26 @@ class RestApi extends Singleton {
      *
      * @return void
      */
-    public function getPluginStatus(\WP_REST_Request $request) {
+    public function getPluginStatus(\WP_REST_Request $request)
+    {
         $this->success(pubjet_plugin_status());
     }
 
     /**
      * @return void
      */
-    public function getPluginVersion(\WP_REST_Request $request) {
+    public function getPluginVersion(\WP_REST_Request $request)
+    {
         $this->success(['version' => PUBJ()->getVersion(),]);
     }
 
     /**
      * @return void
      */
-    public function updateReportage(\WP_REST_Request $request) {
+    public function updateReportage(\WP_REST_Request $request)
+    {
         try {
-            $reportage_id      = $request->get_param('reportageId');
+            $reportage_id = $request->get_param('reportageId');
             $reportage_post_id = pubjet_find_post_id_by_reportage_id(pubjet_isset_value($reportage_id));
             pubjet_log("====== Update Reportage Post ======");
             pubjet_log('Reportage ID: ' . $reportage_id);
@@ -241,8 +253,8 @@ class RestApi extends Singleton {
                 $this->error(pubjet__('rep-not-found'), 404);
             }
 
-            $reportage_post        = get_post($reportage_post_id);
-            $reportage             = (object)$request->get_json_params();
+            $reportage_post = get_post($reportage_post_id);
+            $reportage = (object)$request->get_json_params();
             $reportage->wp_post_id = $reportage_post->ID;
             pubjet_log($reportage);
 
@@ -254,7 +266,7 @@ class RestApi extends Singleton {
                 }
                 $sentry_error = is_wp_error($wp_post_id) ? $wp_post_id->get_error_message() : 'خطای نامشخصی در فرایند ثبت نوشته رپورتاژ رخ داده است.';
                 pubjet_log_sentry($sentry_error, [
-                    'reportage_id'    => pubjet_isset_value($reportage->id),
+                    'reportage_id' => pubjet_isset_value($reportage->id),
                     'reportage_title' => pubjet_isset_value($reportage->title),
                 ]);
                 $this->error($sentry_error, 400);
@@ -273,7 +285,8 @@ class RestApi extends Singleton {
      *
      * @return void
      */
-    public function findBacklink(\WP_REST_Request $request) {
+    public function findBacklink(\WP_REST_Request $request)
+    {
         $backlink_id = $request->get_param('backlinkId');
         pubjet_log('===== Get Backlink Details =====');
         $backlink_row = pubjet_find_backlink_by_id($backlink_id);
@@ -286,7 +299,8 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function createBacklink(\WP_REST_Request $request) {
+    public function createBacklink(\WP_REST_Request $request)
+    {
         $backlink_json_data = (object)$request->get_json_params();
         /**
          * The pubjet_create_backlink action.
@@ -301,7 +315,8 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function createReportage(\WP_REST_Request $request) {
+    public function createReportage(\WP_REST_Request $request)
+    {
         try {
             $reportage = (object)$request->get_json_params();
             pubjet_log(['Create Reportage By Rest Api : ' => $reportage]);
@@ -324,10 +339,11 @@ class RestApi extends Singleton {
      *
      * @return void
      */
-    public function registerRoute($namespace, $callback, $method = \WP_REST_Server::READABLE, $args = null) {
+    public function registerRoute($namespace, $callback, $method = \WP_REST_Server::READABLE, $args = null)
+    {
         register_rest_route('pubjet/v1', "/$namespace", [
-            'methods'             => $method,
-            'callback'            => [$this, $callback],
+            'methods' => $method,
+            'callback' => [$this, $callback],
             'permission_callback' => [$this, 'permissonCallback'],
         ]);
     }
@@ -337,7 +353,8 @@ class RestApi extends Singleton {
      *
      * @return boolean
      */
-    public function permissonCallback(\WP_REST_Request $request) {
+    public function permissonCallback(\WP_REST_Request $request)
+    {
         if (!$this->isTokenValid($request)) {
             wp_send_json_error(pubjet__('invalid-token'), 401);
         }
@@ -347,7 +364,8 @@ class RestApi extends Singleton {
     /**
      * @return bool
      */
-    public function isTokenValid(\WP_REST_Request $request) {
+    public function isTokenValid(\WP_REST_Request $request)
+    {
         if (pubjet_is_dev_mode()) {
             return true;
         }
@@ -362,7 +380,8 @@ class RestApi extends Singleton {
     /**
      * @return void
      */
-    public function finishRequest() {
+    public function finishRequest()
+    {
         ignore_user_abort(true);
 
         if (!headers_sent()) {
