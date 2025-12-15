64c47c0ce1e1eb550b282232bea051b1f19a5660
mojtaba <mojtaba.m2021@gmail.com>
2025-12-15 14:33:20 +0330

    add 5.4.5 version and changelogs

--- pubjet.php
+++ pubjet.php
@@ -6,7 +6,7 @@
     Author URI:  https://triboon.net
     License: GPL v2 or later
     License URI: http://www.gnu.org/licenses/gpl-2.0.txt
-    Version: 5.4.4
+    Version: 5.4.5
 */
 
 use triboon\pubjet\includes\enums\EnumOldOptions;
